00d548ed244eff27af7c94836a58ccbff90c699a
unknown <vincent.rollot@epfedu.fr>
2015-02-05 17:15:19 +0100

    poule

---
+++ vincent_work/models/Team.java
@@ -0,0 +1,96 @@
+package com.ManagerTournament.models;
+
+import java.util.List;
+
+public class Team {
+	String name;
+	String desc;
+	List<Player> list_player; 
+	//la premiere colonne correspond au nombre de victoire 
+	//la deuxième colonne correspond au nombre de défaite
+	//la troisième colonne correspond au nombre de match nul
+	int score[]= new int[3];
+	int goalaverage=0;
+	
+	
+
+
+	//Constructor
+	//
+	public Team(String name) {
+		this.name = name;
+		goalaverage = 0;
+		for(int i=0; i<3;i++){
+			score[i]=0;
+		}
+	}
+	//
+	
+	//Methods
+	//
+	public void print_name(){
+		System.out.println(this.name);
+	}
+	
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+
+	public List<Player> getList_player() {
+		return list_player;
+	}
+	public int[] getscore(){
+		return score;
+	}
+	
+	public void setList_player(List<Player> list_player) {
+		this.list_player = list_player;
+	}
+	public void win_game(){
+		score[0]++;
+	}
+	public void lose_game(){
+		score[1]++;
+	}
+	public void tied_game(){
+		score[2]++;
+	}
+	public void set_win_lose_goalaverage( Team team2, int scoreteam1, int scoreteam2){
+		if(scoreteam1 > scoreteam2){
+			this.win_game();
+			team2.lose_game();
+		}
+		else if(scoreteam1 < scoreteam2){
+			this.lose_game();
+			team2.win_game();
+		}
+		else{
+			this.tied_game();
+			team2.tied_game();
+		}
+		this.setGoalaverage(scoreteam1 - scoreteam2);
+		team2.setGoalaverage(scoreteam2 - scoreteam1);
+	}
+	
+	public int calculate_score(){
+		return score[0]*3+ score[2];
+	}
+	public int getGoalaverage() {
+		return goalaverage;
+	}
+
+	public void setGoalaverage(int goalaverage_match) {
+		this.goalaverage = goalaverage_match + this.goalaverage;
+	}
+	//
+	
+}
